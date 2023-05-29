9504c396d5a638c1b24ba7caf6ba43934c15f647
lorhanymoraes <107012585+lorhanymoraes@users.noreply.github.com>
2023-05-29 20:33:14 -0300

    parse data between screens

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -99,6 +99,11 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.star),
+          onPressed: () {
+            Navigator.of(context).pop(meal.title);
+          }),
     );
   }
 }
