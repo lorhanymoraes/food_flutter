dbc6ba996cec7f3668481a076c3ae246f2cc371b
lorhanymoraes <107012585+lorhanymoraes@users.noreply.github.com>
2023-05-29 22:03:04 -0300

    setup favorites

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:food_flutter/components/meal_item.dart';
+
+import '../models/meals.dart';
 
 class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({super.key});
+  final List<Meal> favoriteMeals;
+
+  const FavoriteScreen(this.favoriteMeals);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Minhas refeições favoritas!'),
-    );
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('Nenhuma refeição foi marcada como favorita!'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(favoriteMeals[index]);
+        },
+      );
+    }
   }
 }
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import '../models/meals.dart';
 
 class MealDetailScreen extends StatelessWidget {
+  final Function(Meal) onToggleFavorite;
+  final bool Function(Meal) isFavorite;
+
+  const MealDetailScreen(this.onToggleFavorite, this.isFavorite);
   Widget _createSectionTitle(BuildContext context, String title) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
@@ -100,9 +104,9 @@ class MealDetailScreen extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.star),
+          child: Icon(isFavorite(meal) ? Icons.star : Icons.star_border),
           onPressed: () {
-            Navigator.of(context).pop(meal.title);
+            onToggleFavorite(meal);
           }),
     );
   }
